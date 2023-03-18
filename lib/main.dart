d46b44a7b5544a26ac3babd06719bf465568bd9a
DerorLamHaoZher <derorlamhaozher86@gmail.com>
2023-03-18 22:38:21 +0800

    AskMeAnythingBall

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+
+
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BallPage());
+}
+
+
+
+class BallPage extends StatelessWidget {
+  const BallPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      home : Scaffold(
+        backgroundColor: Colors.cyanAccent,
+
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        title: const Center(
+            child: Text('Ask Me Anything')),
+      ),
+        body:  const Ball(),
+
+
+    ),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  const Ball({Key? key}) : super(key: key);
+
+  @override
+  State<Ball> createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+ static int ballNumber=0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+       child : TextButton(
+         onPressed: (){
+           setState(() {
+             ballNumber=Random().nextInt(5)+1;
+           });
+         print('Button is pressed and it is Number $ballNumber');
+         },
+        child: Image.asset('images/ball$ballNumber.png',
+        ),
+       ),
+      ),
+    );
+  }
+}
+
+
